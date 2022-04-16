8ffb8ab1dc80b6d9a6655ae9d9214d8618dfa195
Timur Garipov <tr.garipov@yandex.ru>
2022-04-17 01:22:35 +0300

    allFuns

---
+++ src/test/kotlin/ChatServiceTest.kt
@@ -0,0 +1,50 @@
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class ChatServiceTest {
+
+    @Test
+    fun getChats() {
+    }
+
+    @Test
+    fun setChats() {
+    }
+
+    @Test
+    fun getId() {
+    }
+
+    @Test
+    fun setId() {
+    }
+
+    @Test
+    fun sendMessage() {
+    }
+
+    @Test
+    fun testGetChats() {
+    }
+
+    @Test
+    fun readChat() {
+    }
+
+    @Test
+    fun getUnreadChatsCount() {
+    }
+
+    @Test
+    fun deleteChat() {
+    }
+
+    @Test
+    fun editMessage() {
+    }
+
+    @Test
+    fun deleteMessage() {
+    }
+}
